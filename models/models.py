0e10a267d3a5394f557c1649d9c180e35337fcbc
Levis <levislibra@hotmail.com>
2017-09-28 20:31:06 -0300

    Update

--- models/models.py
+++ models/models.py
@@ -2,14 +2,17 @@
 
 from openerp import models, fields, api
 
-# class payment_cost(models.Model):
-#     _name = 'payment_cost.payment_cost'
+class payment_cost(models.Model):
+	_inherit = 'account.payment'
+	_name = 'account.payment'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+	payment_cost = fields.Boolean('¿Tiene costo?')
+	cost_communication = fields.Char('Motivo')
+	cost_journal_id = fields.Many2one('account.journal', 'Diario')
+	cost_percentage = fields.Float('Porcentaje del monto')
+	cost_amount = fields.Float('Monto del costo', compute="_value_cost_amount")
+
+	@api.depends('cost_percentage')
+	def _value_cost_amount(self):
+		if self.amount > 0 and self.cost_percentage > 0:
+			self.cost_amount = self.amount * (self.cost_percentage / 100)
