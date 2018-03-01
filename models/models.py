2a420e7e1d73ce7e7cbf9020283dc73d04e8b57b
Levis <levislibra@hotmail.com>
2018-03-01 12:03:08 -0300

    Update many issues

--- models/models.py
+++ models/models.py
@@ -6,17 +6,19 @@ class payment_cost(models.Model):
 	_inherit = 'account.payment'
 	_name = 'account.payment'
 
-	payment_cost = fields.Boolean('¿Tiene costo?')
+	payment_cost = fields.Boolean('¿Tiene costo?', related='payment_desc')
+	payment_desc = fields.Boolean('¿Tiene descuento?')
 	cost_communication = fields.Char('Motivo')
-	cost_journal_id = fields.Many2one('account.journal', 'Diario')
-	cost_percentage = fields.Float('Porcentaje del monto')
-	cost_amount = fields.Float('Monto del costo', compute="_value_cost_amount")
-	cost_move_id = fields.Many2one('account.move', 'Asiento del costo', default=None)
+	cost_journal_id = fields.Many2one('account.journal', 'Diario', domain="[('type', '=', 'purchase')]")
+	cost_percentage = fields.Float('Porcentaje')
+	cost_amount = fields.Float('Monto')
+	cost_move_id = fields.Many2one('account.move', 'Asiento', default=None)
 	cost_move_boolean = fields.Boolean(default=False)
 
 	@api.depends('cost_percentage')
-	def _value_cost_amount(self):
-		if self.amount > 0 and self.cost_percentage > 0:
+	@api.onchange('cost_percentage')
+	def _change_cost_amount(self):
+		if (self.amount > 0 or self.readonly_amount > 0) and self.cost_percentage > 0:
 			self.cost_amount = self.amount * (self.cost_percentage / 100)
 
 	@api.one
@@ -32,60 +34,58 @@ class payment_cost(models.Model):
 	def confirm_cost(self):
 		if self.payment_cost == True and self.state == 'posted' and len(self.cost_move_id) == 0:
 			line_ids = []
-			if self.payment_type == "outbound" or self.payment_type == "transfer":
-				# Cuando realizamos el pago a un proveedor o una transferencia interna
-				# el costo del pago nos genera un egreso.
-				account_id = None
-				if self.payment_type == "outbound":
-					account_id = self.partner_id.property_account_payable_id.id
-				elif self.payment_type == "transfer":
-					account_id = self.destination_journal_id.default_debit_account_id.id
-				# Registramos el costo a favor del proveedor o decrementamos la cuenta destino
-				aml = {
-				    'date': self.payment_date,
-				    'account_id': account_id,
-				    'name': self.cost_communication,
-				    'partner_id': self.partner_id.id,
-				    'credit': self.cost_amount,
-				    #'journal_id': self.
-				}
-				line_ids.append((0,0,aml))
+			if self.payment_type == "outbound":
+				# Cuando realizamos el pago a un proveedor o un cliente
+				
+				if self.partner_type == "supplier":
+					# Pago a un proveedor - genera un costo
+					# Registramos el monto a favor del proveedor, lo cual nos generara un
+					# costo
+					aml = {
+					    'date': self.payment_date,
+					    'account_id': self.partner_id.property_account_payable_id.id,
+					    'name': self.cost_communication,
+					    'partner_id': self.partner_id.id,
+					    'credit': self.cost_amount,
+					    'journal_id': self.cost_journal_id.id,
+					}
+					line_ids.append((0,0,aml))
 
-				# Registramos el costo como gasto en el diario seleccionado
-				aml2 = {
-				    'date': self.payment_date,
-				    'account_id': self.cost_journal_id.default_debit_account_id.id,
-				    'name': self.cost_communication,
-				    'partner_id': self.partner_id.id,
-				    'debit': self.cost_amount,
-				    #'journal_id': self.
-				}
-				line_ids.append((0,0,aml2))
-			elif self.payment_type == "inbound":
-				# Cuando registramos el pago de un cliente
-				# el costo del pago nos genera un ingreso.
+					# Registramos el costo como gasto en el diario seleccionado
+					aml2 = {
+					    'date': self.payment_date,
+					    'account_id': self.cost_journal_id.default_debit_account_id.id,
+					    'name': self.cost_communication,
+					    'partner_id': self.partner_id.id,
+					    'debit': self.cost_amount,
+					    'journal_id': self.cost_journal_id.id,
+					}
+					line_ids.append((0,0,aml2))
+				elif self.partner_type == "customer":
+					# Pago a un cliente - genera un descuento
 
-				# Registramos el costo como un ingreso en el diario seleccionado
-				aml = {
-				    'date': self.payment_date,
-				    'account_id': self.cost_journal_id.default_debit_account_id.id,
-				    'name': self.cost_communication,
-				    'partner_id': self.partner_id.id,
-				    'credit': self.cost_amount,
-				    #'journal_id': self.
-				}
-				line_ids.append((0,0,aml))
+					# Registramos el monto a favor del cliente, lo cual genera un
+					# descuento
+					aml = {
+					    'date': self.payment_date,
+					    'account_id': self.partner_id.property_account_receivable_id.id,
+					    'name': self.cost_communication,
+					    'partner_id': self.partner_id.id,
+					    'credit': self.cost_amount,
+					    'journal_id': self.cost_journal_id.id,
+					}
+					line_ids.append((0,0,aml))
 
-				# Registramos el costo disminuyendo la cuenta del cliente
-				aml2 = {
-				    'date': self.payment_date,
-				    'account_id': self.partner_id.property_account_receivable_id.id,
-				    'name': self.cost_communication,
-				    'partner_id': self.partner_id.id,
-				    'debit': self.cost_amount,
-				    #'journal_id': self.
-				}
-				line_ids.append((0,0,aml2))
+					# Registramos el costo disminuyendo la cuenta del cliente
+					aml2 = {
+					    'date': self.payment_date,
+					    'account_id': self.cost_journal_id.default_debit_account_id.id,
+					    'name': self.cost_communication,
+					    'partner_id': self.partner_id.id,
+					    'debit': self.cost_amount,
+					    'journal_id': self.cost_journal_id.id,
+					}
+					line_ids.append((0,0,aml2))
 
 
 			# create move
